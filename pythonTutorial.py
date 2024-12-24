3f1e3d6695ab84676998a4945952ec9be55833d6
Harsh Mishra <harshmishra2997@gmail.com>
2024-12-24 23:48:33 +0530

    string sclicing done

---
+++ pythonTutorial.py
@@ -0,0 +1,43 @@
+# printing string with space betwwen them;
+a= 'bruce'
+b= 'banner' 
+print (a + ' '+ b)
+
+# string concatination;
+greeting= "hello"
+print(greeting + "world");
+
+# ue of (**) operator;
+a= 10;
+print (a**2);
+
+# python program to calculate Simple Intrest;
+p=4 
+r=2 
+t=10
+si=(p * r * t)/ 100
+print(si)
+
+# python program to convert celcius to fahrenheit;
+celsius= 100
+fahrenheit= (celsius * 9/5)+ 32
+print(fahrenheit)
+
+#  addition of two number(input from user);
+a = int (input('enter a number:'))
+b= int (input ("enter a number:"))
+sum= a+b;
+print(sum);
+
+# average of two number(input form user);
+a= float(input("enter a number:"))
+b= float(input("enter a number:"))
+average= (a+b)/2
+print(average)
+
+# string sclicing
+string= "hello"
+print(string[0:3])
+
+string= "hello";
+print(string[4:5]);
