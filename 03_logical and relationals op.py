0a02619187428683e61cb0bdbbc07ab016ec0a78
Harsh <168585844+Typecaster12@users.noreply.github.com>
2025-01-07 19:39:29 +0530

    Add files via upload

---
+++ 03_logical and relationals op.py
@@ -0,0 +1,11 @@
+# age = int(input("Enter your age:"))
+# if (age > 34 and age < 54): ##both conditions must be true only then this whole statement proves to be true
+#     print("You can work with us")
+# else:
+#     print("You cannot work with us")
+
+age = int(input("Enter your age: "))
+if(age<20 or age<56):
+    print("You can work with us")
+else:
+    print("You cannot work with us")
---
+++ tut2.py
@@ -0,0 +1,30 @@
+# age= int(input("enter your age:"));
+# if(age>=18):
+    # print("true")
+# else:
+    # print("false")
+# num= int(input("enter a number"))
+# if(num>0):
+    # print("positive")
+# elif(num<0):
+    # print("negative")
+# else: 
+    # print("num is 0")
+# num= int(input("enter a number"))
+# if((num%2) == 0):
+    # print(num,"is even");
+# else:
+    # print(num,"is odd")
+# year = int(input('enter a number'));
+# if((year % 4 ==  0 and year% 100!= 0) or (year%400==0)):
+#     print('year is a leap year');
+# else:
+    # print('not a leap year');
+num = int(input('enter a number'))
+if(num % 5== 0 and num % 7== 0 ):
+    print('number is divisible')
+else:
+    print('it is not divisible')
+
+
+        
