b823f034615196855390de57b6b933b2d788360e
Harsh <168585844+Typecaster12@users.noreply.github.com>
2025-01-07 19:38:04 +0530

    Add files via upload

---
+++ 01_conditionals.py
@@ -0,0 +1,51 @@
+from turtle import done
+
+# IF-ELIF-ELSE leared
+
+# a = 45
+# if(a>10):
+#     print("The value of a is greater then 10")
+# else:
+#     print("The value of a is less then 10")
+# print("Done")
+
+# r = 2
+# if(r>10):
+#     print("The value of r is greater then 10")
+# elif(r>20):
+#     print("The value of r is greatr then 20")
+# elif(r<50):
+#     print('The value of r is less then 50')
+# else:
+#     print("The value is smaller then all the value above")
+# print("Done")
+
+
+# b = 2
+# if(b>10):
+#     print("The value of b is greater then 10")
+# elif(b>20):
+#     print("The value of b is greatr then 20")
+# elif(b>50):
+#     print('The value of b is less then 50')
+# else:
+#     print("b is not greater than 10,20,50")
+
+# print("Done")
+
+##multiple if statements 
+
+o = 10
+if(o>20):
+    print("Yess o is grearett than 20")
+if(o<20):
+    print("The value of o is smaller than 20")
+if(o<2):
+    print("The value of o is smaller than 2")
+if(o<30):
+    print("The value of o is smaller than 30")
+else:
+    print("The value is 10 only")
+print("Done")
+
+print("Done")
